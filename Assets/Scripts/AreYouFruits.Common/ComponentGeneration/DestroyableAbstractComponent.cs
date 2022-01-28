f0fbe0d638b760ae621c9ad1254d8f9ae6f0039a
Sergey Ovchinnikov <unknown.musician.t@gmail.com>
2022-01-28 19:13:05 +0200

    Add DestroyableAbstractComponent

---
+++ Assets/Scripts/AreYouFruits.Common/ComponentGeneration/DestroyableAbstractComponent.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AreYouFruits.Common.ComponentGeneration
+{
+    public abstract class DestroyableAbstractComponent<T> : AbstractComponent<T> where T : class, IDisposable
+    {
+        protected void OnDestroy() => HeldItem.Dispose();
+    }
+}
