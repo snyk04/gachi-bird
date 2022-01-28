acd4f9cf2d4df5e021a17f730e0bb4d479e93b78
Sergey Ovchinnikov <unknown.musician.t@gmail.com>
2022-01-28 11:36:19 +0200

    add collections extensions

---
+++ Assets/Scripts/AreYouFruits.Common/Collections/GenericExtensions.cs
@@ -0,0 +1,78 @@
+﻿#nullable enable
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AreYouFruits.Common.Collections.GenericExtensions
+{
+    public static class GenericExtensions
+    {
+        public static TDictionary Foreach<TKey, TValue, TDictionary>(this TDictionary dictionary, Action<TKey, TValue> action)
+            where TDictionary : IDictionary<TKey, TValue>
+        {
+            foreach ((TKey key, TValue value) in dictionary)
+            {
+                action.Invoke(key, value);
+            }
+            
+            return dictionary;
+        }
+
+        public static TEnumerable Foreach<T, TEnumerable>(this TEnumerable collection, Action<T> action)
+            where TEnumerable : IEnumerable<T>
+        {
+            foreach (T element in collection)
+            {
+                action.Invoke(element);
+            }
+            
+            return collection;
+        }
+        
+        public static TEnumerable ForeachNonGeneric<TEnumerable>(this TEnumerable collection, Action<object> action)
+            where TEnumerable : IEnumerable
+        {
+            foreach (object element in collection)
+            {
+                action.Invoke(element);
+            }
+            
+            return collection;
+        }
+        
+        public static TReadOnlyCollection For<T, TReadOnlyCollection>(this TReadOnlyCollection array, Action<int> action)
+            where TReadOnlyCollection : IReadOnlyCollection<T>
+        {
+            for (int i = 0; i < array.Count; i++)
+            {
+                action.Invoke(i);
+            }
+            
+            return array;
+        }
+        
+        public static TReadOnlyList For<T, TReadOnlyList>(this TReadOnlyList array, Action<int, T> action)
+            where TReadOnlyList : IReadOnlyList<T>
+        {
+            for (int i = 0; i < array.Count; i++)
+            {
+                action.Invoke(i, array[i]);
+            }
+            
+            return array;
+        }
+        
+        public static void SafeAddToValueCollection<TKey, TValue, TCollection, TDictionary>(this TDictionary dictionary, TKey key, TValue value)
+            where TCollection : ICollection<TValue>, new()
+            where TDictionary : IDictionary<TKey, TCollection>
+        {
+            if (!dictionary.TryGetValue(key, out TCollection valueList))
+            {
+                valueList = dictionary[key] = new TCollection();
+            }
+            
+            valueList.Add(value);
+        }
+    }
+}
---
+++ Assets/Scripts/AreYouFruits.Common/Collections/OtherExtensions.cs
@@ -0,0 +1,44 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+
+namespace AreYouFruits.Common.Collections
+{
+    public static class OtherExtensions
+    {
+        public static T[,] For<T>(this T[,] array, Action<T> action) => ForArray<T, T[,]>(array, action);
+        public static T[,,] For<T>(this T[,,] array, Action<T> action) => ForArray<T, T[,,]>(array, action);
+        public static T[,,,] For<T>(this T[,,,] array, Action<T> action) => ForArray<T, T[,,,]>(array, action);
+        public static T[,,,,] For<T>(this T[,,,,] array, Action<T> action) => ForArray<T, T[,,,,]>(array, action);
+
+        // todo: Check.
+        private static TArray ForArray<TElement, TArray>(Array array, Action<TElement> action)
+            where TArray : class
+        {
+            For(array, new int[array.Rank], obj => action.Invoke((TElement)obj));
+            
+            return (array as TArray)!;
+        }
+
+        // todo: Check.
+        private static void For(Array array, int[] indices, Action<object> action, int dimension = 0)
+        {
+            Action nextAction = dimension == indices.Length - 1
+                ? () => action.Invoke(array.GetValue(indices))
+                : (Action)(() => For(array, indices, action, dimension + 1));
+
+            for (int i = 0; i < array.GetLength(dimension); i++)
+            {
+                indices[dimension] = i;
+                nextAction();
+            }
+        }
+        
+        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> pair, out TKey key, out TValue value)
+        {
+            key = pair.Key;
+            value = pair.Value;
+        }
+    }
+}
