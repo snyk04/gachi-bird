357f09e18d3922e4db6f08c3cc025fd6fde5080e
Sergey Ovchinnikov <unknown.musician.t@gmail.com>
2022-01-28 19:07:29 +0200

    Add AbstractComponent

---
+++ Assets/Scripts/AreYouFruits.Common/ComponentGeneration/AbstractComponent.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+#nullable enable
+
+namespace AreYouFruits.Common.ComponentGeneration
+{
+    public abstract class AbstractComponent<T> : MonoBehaviour where T : class
+    {
+        private T? _heldItem;
+        private bool _isStartedInitialization = false;
+
+        public T HeldItem
+        {
+            get
+            {
+                if (_heldItem != null)
+                {
+                    return _heldItem;
+                }
+
+                if (_isStartedInitialization)
+                {
+                    throw new RecursiveInitializationException();
+                }
+
+                _isStartedInitialization = true;
+                _heldItem = Create();
+
+                return _heldItem;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            _ = HeldItem;
+        }
+
+        protected abstract T Create();
+    }
+}
---
+++ Assets/Scripts/AreYouFruits.Common/ComponentGeneration/RecursiveInitializationException.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+
+using System;
+
+namespace AreYouFruits.Common.ComponentGeneration
+{
+    public class RecursiveInitializationException : InvalidOperationException
+    {
+        private const string ExceptionMessage =
+            "There are bilateral dependencies. (This was created to avoid Recursion Exception)";
+
+        public RecursiveInitializationException() : base(ExceptionMessage) { }
+        public RecursiveInitializationException(string message) : base(message) { }
+    }
+}
