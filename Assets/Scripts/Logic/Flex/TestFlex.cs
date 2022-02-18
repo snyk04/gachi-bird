ad305b06850865665b8f5c2ececa7d14b82f4513
Sergey Ovchinnikov <unknown.musician.t@gmail.com>
2022-02-18 13:54:11 +0200

    Fix TestFlex

--- Assets/Scripts/Logic/Flex/TestFlex.cs
+++ Assets/Scripts/Logic/Flex/TestFlex.cs
@@ -5,24 +5,22 @@ using UnityEngine;
 
 namespace GachiBird.Flex
 {
-    public class TestFlex : IFlexModeHandler
+    public class TestFlex : IFlexModeHandler, IDisposable
     {
         public event Action<BoosterInfo>? OnFlexModeStart;
         public event Action? OnFlexModeEnd;
 
-        private readonly BoosterInfo _boosterInfo;
-
         public TestFlex(BoosterInfo boosterInfo)
         {
-            _boosterInfo = boosterInfo;
-
-            StartFlexMode(new GameObject(), null, _boosterInfo);
+            StartFlexMode(new GameObject(), null, boosterInfo);
         }
 
-        public async void StartFlexMode(GameObject boosterObject, IBooster booster, BoosterInfo boosterInfo)
+        public async void StartFlexMode(GameObject boosterObject, IBooster? booster, BoosterInfo boosterInfo)
         {
             await Tasks.DelaySeconds(1);
             OnFlexModeStart?.Invoke(boosterInfo);
         }
+
+        public void Dispose() => OnFlexModeEnd?.Invoke();
     }
 }
