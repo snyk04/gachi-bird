ef4eb31a3a07920782ad01e124c746c7bad221de
Sergey Ovchinnikov <unknown.musician.t@gmail.com>
2022-03-08 15:40:33 +0200

    Improve SerializedInterface

--- Assets/Scripts/AreYouFruits.Common/ComponentGeneration/SerializedInterface.cs
+++ Assets/Scripts/AreYouFruits.Common/ComponentGeneration/SerializedInterface.cs
@@ -29,40 +29,110 @@ namespace AreYouFruits.Common.ComponentGeneration
     [CustomPropertyDrawer(typeof(SerializedInterface<>))]
     public class SerializedObjectDrawer : PropertyDrawer
     {
+        private enum SerializeType { Interface = 0, Object, }
+
+        private const int PopupWidth = 50;
+
+        private static readonly string[] _possibleSerializeTypeNames = Enum.GetNames(typeof(SerializeType));
+
+        private SerializeType _serializeType = SerializeType.Interface;
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.BeginProperty(position, label, property);
 
             object serializedInterface = GetTargetObjectOfProperty(property)!;
 
-            Type interfaceType = (Type)serializedInterface.GetType()!
-                .GetProperty("InterfaceType", BindingFlags.Instance | BindingFlags.NonPublic)!
-                .GetMethod.Invoke(serializedInterface, null);
-            
+            Type interfaceType =
+                (Type)serializedInterface.GetType()!.GetProperty(
+                    "InterfaceType",
+                    BindingFlags.Instance | BindingFlags.NonPublic
+                )!.GetMethod.Invoke(serializedInterface, null);
+
             SerializedProperty objectProperty = property.FindPropertyRelative("_object");
-            
-            objectProperty.objectReferenceValue = EditorGUI.ObjectField(position, label, objectProperty.objectReferenceValue, interfaceType, true);
+
+            Rect popupPosition = new Rect(
+                new Vector2(position.max.x - PopupWidth, position.position.y),
+                new Vector2(PopupWidth, position.size.y)
+            );
+
+            position = new Rect(position.position, position.size - PopupWidth * Vector2.right);
+
+            _serializeType = (SerializeType)EditorGUI.Popup(
+                popupPosition,
+                (int)_serializeType,
+                _possibleSerializeTypeNames
+            );
+
+            switch (_serializeType)
+            {
+                case SerializeType.Interface:
+                    OnGuiAsInterface(position, objectProperty, label, interfaceType);
+                    break;
+                case SerializeType.Object:
+                    OnGuiAsGameObject(position, objectProperty, label, interfaceType);
+                    break;
+                default: throw new ArgumentOutOfRangeException();
+            }
 
             EditorGUI.EndProperty();
         }
-        
+
+        private void OnGuiAsInterface(
+            Rect position, SerializedProperty objectProperty, GUIContent label, Type interfaceType
+        )
+        {
+            objectProperty.objectReferenceValue = EditorGUI.ObjectField(
+                position,
+                label,
+                objectProperty.objectReferenceValue,
+                interfaceType,
+                true
+            );
+        }
+
+        private void OnGuiAsGameObject(
+            Rect position, SerializedProperty objectProperty, GUIContent label, Type interfaceType
+        )
+        {
+            Object obj = EditorGUI.ObjectField(
+                position,
+                label,
+                objectProperty.objectReferenceValue,
+                typeof(Object),
+                true
+            );
+
+            obj = obj switch
+            {
+                GameObject gameObject => gameObject.GetComponent(interfaceType),
+                Component objComponent => objComponent,
+                _ => obj
+            };
+
+            if (obj is null || interfaceType.IsInstanceOfType(obj))
+            {
+                objectProperty.objectReferenceValue = obj;
+            }
+        }
+
         private static object? GetTargetObjectOfProperty(SerializedProperty prop)
         {
             string path = prop.propertyPath.Replace(".Array.data[", "[");
             object? obj = prop.serializedObject.targetObject;
-            
+
             foreach (string element in path.Split('.'))
             {
                 if (element.Contains("["))
                 {
                     string elementName = element.Substring(0, element.IndexOf("[", StringComparison.Ordinal));
-                    
+
                     int index = Convert.ToInt32(
-                        element.Substring(
-                                element.IndexOf("[", StringComparison.Ordinal))
-                        .Replace("[", "")
-                        .Replace("]", ""));
-                    
+                        element.Substring(element.IndexOf("[", StringComparison.Ordinal))
+                            .Replace("[", "")
+                            .Replace("]", "")
+                    );
+
                     obj = GetValueImp(obj, elementName, index);
                 }
                 else
@@ -70,7 +140,7 @@ namespace AreYouFruits.Common.ComponentGeneration
                     obj = GetValueImp(obj, element);
                 }
             }
-            
+
             return obj;
         }
 
@@ -82,7 +152,7 @@ namespace AreYouFruits.Common.ComponentGeneration
             }
 
             IEnumerator enm = enumerable.GetEnumerator();
-            
+
             for (int i = 0; i <= index; i++)
             {
                 if (!enm.MoveNext())
@@ -90,7 +160,7 @@ namespace AreYouFruits.Common.ComponentGeneration
                     return null;
                 }
             }
-            
+
             return enm.Current;
         }
 
@@ -105,15 +175,21 @@ namespace AreYouFruits.Common.ComponentGeneration
 
             while (type != null)
             {
-                FieldInfo? f = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-                
+                FieldInfo? f = type.GetField(
+                    name,
+                    BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance
+                );
+
                 if (f != null)
                 {
                     return f.GetValue(source);
                 }
 
-                PropertyInfo? p = type.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
-                
+                PropertyInfo? p = type.GetProperty(
+                    name,
+                    BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase
+                );
+
                 if (p != null)
                 {
                     return p.GetValue(source, null);
@@ -121,7 +197,7 @@ namespace AreYouFruits.Common.ComponentGeneration
 
                 type = type.BaseType;
             }
-            
+
             return null;
         }
     }
